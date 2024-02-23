11ae01a3127a6207f2599d16a9a252e0e37b9df3
hana <hanadul34@weversecompany.com>
2024-02-23 00:05:36 +0000

    Update tokens!

--- components/themes/tokens.js
+++ components/themes/tokens.js
@@ -1,48 +1,48 @@
 /**
  * Do not edit directly
- * Generated on Thu, 22 Feb 2024 02:49:39 GMT
+ * Generated on Fri, 23 Feb 2024 00:05:36 GMT
  */
 
 module.exports = {
+  "animation": {
+    "comment": "Random tokens",
+    "basics": {
+      "Duration-100": "100ms",
+      "small": [
+        0,
+        0,
+        0.5,
+        1
+      ]
+    }
+  },
   "base": {
     "comment": "All the base color tokens",
     "type": "color",
+    "backgrounds": {
+      "blue": "#6C8FFFF0",
+      "pink": "#FF9BE3"
+    },
     "palette": {
       "primary30": "#4F378B",
       "primary50": "#826BBB",
       "primary70": "#B6A1E9",
       "primary90": "#EADDFF"
-    },
-    "backgrounds": {
-      "light-primary": "#EADDFF",
-      "dark-primary": "#4F378B"
     }
   },
   "sizes": {
     "type": "dimension",
     "base spacing": "0.25rem",
     "digital": {
-      "web": "4rem",
       "devices": {
-        "tablet": "80px",
-        "mobile": "0.25rem"
-      }
+        "mobile": "0.25rem",
+        "tablet": "80px"
+      },
+      "web": "4rem"
     }
   },
   "typography": {
-    "primary font": "Comic Sans MS",
-    "font-weight-default": 350
-  },
-  "animation": {
-    "comment": "Random tokens",
-    "basics": {
-      "small": [
-        0,
-        0,
-        0.5,
-        1
-      ],
-      "Duration-100": "100ms"
-    }
+    "font-weight-default": 350,
+    "primary font": "Comic Sans MS"
   }
 };
