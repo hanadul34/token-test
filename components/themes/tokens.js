5a0a5c485a89831b3ec57fbc85bd672d4b116811
hana <hanadul34@wevwersecompany.com>
2024-03-04 02:22:02 +0000

    Update tokens!

--- components/themes/tokens.js
+++ components/themes/tokens.js
@@ -1,52 +1,14 @@
 /**
  * Do not edit directly
-<<<<<<< HEAD
- * Generated on Tue, 27 Feb 2024 05:18:59 GMT
-=======
- * Generated on Mon, 04 Mar 2024 01:27:12 GMT
->>>>>>> feature/update-zeroheight-tokens-202403040126
+ * Generated on Mon, 04 Mar 2024 02:22:02 GMT
  */
 
 module.exports = {
   "base": {
-    "comment": "All the base color tokens",
-    "type": "color",
-    "palette": {
-      "primary30": "#4F378B",
-      "primary50": "#826BBB",
-      "primary70": "#B6A1E9",
-      "primary90": "#EADDFF"
-    },
-    "backgrounds": {
-      "light-primary": "#EADDFF",
-      "dark-primary": "#4F378B"
-    }
-  },
-  "sizes": {
-    "type": "dimension",
-    "base spacing": "0.25rem",
-    "digital": {
-      "web": "4rem",
-      "devices": {
-        "tablet": "80px",
-        "mobile": "0.25rem"
-      }
-    }
-  },
-  "typography": {
-    "primary font": "Comic Sans MS",
-    "font-weight-default": 350
-  },
-  "animation": {
-    "comment": "Random tokens",
-    "basics": {
-      "small": [
-        0,
-        0,
-        0.5,
-        1
-      ],
-      "Duration-100": "100ms"
+    "background": {
+      "blue": "#6AA8EA",
+      "green": "#A9E880",
+      "yellow": "#F7F334"
     }
   }
 };
