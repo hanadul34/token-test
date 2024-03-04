4eea872fedb20571d26b3e4af65984f8a852913c
hana <hanadul34@wevwersecompany.com>
2024-03-04 02:36:52 +0000

    Update tokens!

--- components/themes/tokens.d.ts
+++ components/themes/tokens.d.ts
@@ -2,48 +2,12 @@ declare const root: RootObject
 export default root
 export interface RootObject {
   base: Base;
-  sizes: Sizes;
-  typography: Typography;
-  animation: Animation;
-}
-export interface Animation {
-  comment: string;
-  basics: Basics;
-}
-export interface Basics {
-  small: number[];
-  'Duration-100': string;
-}
-export interface Typography {
-  'primary font': string;
-  'font-weight-default': number;
-}
-export interface Sizes {
-  type: string;
-  'base spacing': string;
-  digital: Digital;
-}
-export interface Digital {
-  web: string;
-  devices: Devices;
-}
-export interface Devices {
-  tablet: string;
-  mobile: string;
 }
 export interface Base {
-  comment: string;
-  type: string;
-  palette: Palette;
-  backgrounds: Backgrounds;
+  background: Background;
 }
-export interface Backgrounds {
-  'light-primary': string;
-  'dark-primary': string;
-}
-export interface Palette {
-  primary30: string;
-  primary50: string;
-  primary70: string;
-  primary90: string;
+export interface Background {
+  blue: string;
+  green: string;
+  yellow: string;
 }
